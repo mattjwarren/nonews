1fb6f5129fc80c0680f69d871332ee527bed23ec
alienbaby <devnull@localhost>
2012-11-15 17:09:06 +0000

    more db stuff

--- nonews/db/datamodel.py
+++ nonews/db/datamodel.py
@@ -4,7 +4,69 @@ Created on 15 Nov 2012
 @author: GB108544
 '''
 
-from db_elements import Schema,Table,Field
+from db_elements import Schema,Table
+from db_elements import IntegerField, StringField
+
+
+
+
+class Content(Table):
+    id=IntegerField(name='id',
+                    readable_name='Content ID',
+                    size=11,
+                    auto_increment=True,
+                    default=None,
+                    not_null=True)
+    
+    headline=StringField(name='headline',
+                         readable_name="Headline",
+                         size=255,
+                         default=None,
+                         not_null=True)
+    
+    body=StringField(name="body",
+                     readable_name="Contents",
+                     size=8192,
+                     default=None,
+                     not_null=True)
+
+
+class Entity(Table):
+    id=IntegerField(name='id',
+                    readable_name='Content ID',
+                    size=11,
+                    auto_increment=True,
+                    default=None,
+                    not_null=True)
+    
+    name=StringField(name="name",
+                     readable_name="Name",
+                     size=128,
+                     default=None,
+                     not_null=True)
+
+class ContentEntities(Table):
+    
+    id=IntegerField(name='id',
+                    readable_name='Content ID',
+                    size=11,
+                    auto_increment=True,
+                    default=None,
+                    not_null=True)
+
+    primary_key=id
+    
+    content_id=RelatedField(name="content_id",
+                            readable_name="Content ID",
+                            default=None,
+                            not_null=True,
+                            related_to=Content.id)
+
+    entity_id=RelatedField(name="entity_id",
+                           readable_name="Entity ID",
+                           default=None,
+                           not_null=True,
+                           related_to=Entity.id)
 
 
     
--- nonews/db/db_elements.py
+++ nonews/db/db_elements.py
@@ -4,31 +4,53 @@ Created on 15 Nov 2012
 @author: GB108544
 '''
 
-class Datatype(object):
-    """Subclass to represent a SQL datatype"""
-    pass
-
-class Integer(Datatype):
-    pass
-
-class String(Datatype):
-    pass
-
 class Field(object):
-    def __init__(self,
-                 name           =None,
-                 readable_name  =None,
-                 datatype       =None,
-                 size           =None):
+    def __init__(self,**kwargs):        
+        try:
+            self.name=kwargs["name"]
+        except KeyError:
+            raise Exception("Required keyword parameter 'name' not found.")
         
-        self.name=name
-        self.readable_name=readable_name
-        self.datatype=datatype
-        self.size=size
+        try:        
+            self.readable_name=kwargs["readable_name"]
+        except KeyError:
+            self.readable_name=None
+        
+        try:
+            self.default=kwargs["default"]
+        except KeyError:
+            self.default=None
+        
+        try:
+            self.not_null=kwargs["not_null"]
+        except KeyError:
+            self.not_null=False
 
     def create_sql(self):
         pass
-    
+
+class IntegerField(Field):
+    def __init__(self,**kwargs):
+        Field.__init__(self,kwargs)
+        self.datatype=int
+        try:
+            self.auto_increment=kwargs["auto_increment"]
+        except KeyError:
+            self.auto_increment=False
+        try:
+            self.size=kwargs["size"]
+        except KeyError:
+            raise Exception("IntegerField requires 'size' parameter.")
+
+class StringField(Field):
+    def __init__(self,**kwargs):
+        Field.__init__(self,kwargs)
+        self.datatype=str
+        try:
+            self.size=kwargs["size"]
+        except KeyError:
+            raise Exception("StringField requires 'size' parameter.")
+        
 class Table(object):
     def __init__(self,fields=None):
         self.fields=fields
